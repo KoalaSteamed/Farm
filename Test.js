bd44e8df3b117d8a741fa5f00afc3ac60757eeea
KoalaSteamed <42348004+KoalaSteamed@users.noreply.github.com>
2018-08-14 00:42:25 +1000

    Update Test.js

--- Test.js
+++ Test.js
@@ -1126,7 +1126,7 @@ function tryClick(button) {
             if (button.hasClass("farm_icon_disabled") || button.html() == undefined) {
 
                 window.top.UI.ErrorMessage("That button is not selectable. Skipping row...", 500);
-                row.hide();
+                button.closest("tr").hide();
             }
             else {
                 button.click();
