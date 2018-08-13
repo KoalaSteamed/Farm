ec7c1b5f1f7c406173cf78892bc8e0e7f876cb06
KoalaSteamed <42348004+KoalaSteamed@users.noreply.github.com>
2018-08-14 00:18:36 +1000

    Update Test.js
    fix skipping

--- Test.js
+++ Test.js
@@ -1121,7 +1121,7 @@ function tryClick(button) {
             if (button.hasClass("farm_icon_disabled") || button.html() == undefined) {
 
                 window.top.UI.ErrorMessage("That button is not selectable. Skipping row...", 500);
-                button.closest('tr').hide();
+                row.hide();
             }
             else {
                 button.click();
